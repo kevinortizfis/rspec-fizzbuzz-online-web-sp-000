8f40a70904d432219bb97d423e586af5f75a57ab
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-04 20:30:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,3 +5,17 @@ def fizzbuzz(int)
     "Fizz" # Go fizz
   end
 end
+
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+   "FizzBuzz"
+  elsif int % 5 == 0
+     "Buzz"
+  elsif int % 3 == 0
+     "Fizz"
+  else  nil
+  end
+end
+# puts "Welcome to Fizzbuzz. Put a number here!"
+# number = gets.strip
+# num1 = number.to_i
