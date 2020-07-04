054a709ef885bd2dcea5ece9bab7b1a5770e246a
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-04 20:05:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
     puts "Fizz"
   elsif int % 5 == 0
     puts "Buzz"
-  elsif int.remainder(3) == 0 && number.remainder(5) == 0
+  elsif int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
   else puts nil
   end
