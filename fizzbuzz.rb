553f2b8e0546d790a44496518686300f3f018e66
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-04 20:27:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,19 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-  puts "FizzBuzz"
-  elsif int % 5 == 0
-    puts "Buzz"
-  elsif int % 3 == 0
-    puts "Fizz"
-  else puts nil
-  end
 end
-print fizzbuzz(3)
-print fizzbuzz(5)
-print fizzbuzz(15)
-print fizzbuzz(4)
 # puts "Welcome to Fizzbuzz. Put a number here!"
 # number = gets.strip
 # num1 = number.to_i
