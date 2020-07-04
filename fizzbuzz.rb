9d935dfa6047f5dc41274c948bb6763b7e6ca14f
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-04 20:03:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if number.remainder(3) == 0
+  if int.remainder(3) == 0
     puts "Fizz"
-  elsif number.remainder(5) == 0
+  elsif int.remainder(5) == 0
     puts "Buzz"
-  elsif= number.remainder(3) == 0 && number.remainder(5) == 0
+  elsif= int.remainder(3) == 0 && number.remainder(5) == 0
     puts "FizzBuzz"
     else puts nil
   end
