724990fa0b0954c7245d76e3cdab29d42753e238
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-04 20:14:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  div = int.to_i
-  if div % 3 == 0 && div % 5 == 0
+  if int % 3 == 0 && int % 5 == 0
   puts "FizzBuzz"
   elsif int % 5 == 0
     puts "Buzz"
