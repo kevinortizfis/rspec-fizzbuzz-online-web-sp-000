f40a5a7240673d244b1adbc98def36191381a463
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-04 20:30:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-  puts "FizzBuzz"
-  elsif int % 5 == 0
-    puts "Buzz"
-  elsif int % 3 == 0
-    puts "Fizz"
-  else puts nil
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
   end
 end
-# puts "Welcome to Fizzbuzz. Put a number here!"
-# number = gets.strip
-# num1 = number.to_i
+
